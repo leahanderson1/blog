ca4145bdc74e25e6b865a91320c8eef6fa4950e0
Oliver Anderson <144633084+OtterCodes101@users.noreply.github.com>
2024-01-23 18:27:06 +0000

    thing

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://otter.is-a.dev/blog", // replace this with your deployed domain
+  website: "https://otter.is-a.dev", // replace this with your deployed domain
   author: "Oliver Anderson",
   desc: "silly blog :3",
   title: "Otter's Blog",
